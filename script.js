3dd2a11398cd832d970c0938f11e2a2a6752d118
Ana Laura <70218957+laurana1991@users.noreply.github.com>
2023-07-09 20:03:13 -0300

    Add files via upload
    Agregue el menu hamburguesa y el link a la API

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+
+//Display mobile menu
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
+
+
+
 // Obtener el JSON de usuarios del LocalStorage
 const usuariosJSON = localStorage.getItem('usuarios');
 let usuarios = [];
